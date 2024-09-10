42f692ad9b9a05f922e34e5306207ec0a0207605
Lewis <lewisomogi01@gmail.com>
2024-09-10 08:13:15 +0300

    learning looping day2

---
+++ forloops.py
@@ -0,0 +1,8 @@
+# for _ in range(3):
+#     print("meow") 
+while True:
+    n = int(input("What's n? "))
+    if n < 0:
+        continue
+    else:
+        break
---
+++ while.py
@@ -0,0 +1,7 @@
+# while loops
+i = 0
+while i < 3:
+    print("meow")
+    i += 1
+    # Notice how changing the operator to i < 3 allows our code to function as intended.
+    # We begin by counting with 0 and it iterates through our loop three times, producing three meows. Also, notice how i += 1 is the same as saying i = i + 1.
